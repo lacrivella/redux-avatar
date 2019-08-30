17595376394da88a3aa531a100daa832348cb117
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-30 15:22:10 -0700

    refactored to use promises instead of thunk

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -2,13 +2,19 @@ import { getCharacters } from '../services/AvatarApi';
 
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
-export const fetchCharacters = () => dispatch => {
-  dispatch({ type: FETCH_CHARACTERS_LOADING });
-  getCharacters()
-    .then(character => {
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: character
-      });
-    });
-};
+// export const fetchCharacters = () => dispatch => {
+//   dispatch({ type: FETCH_CHARACTERS_LOADING });
+//   getCharacters()
+//     .then(character => {
+//       dispatch({
+//         type: FETCH_CHARACTERS,
+//         payload: character
+//       });
+//     });
+// };
+
+export const fetchCharacters = () => ({
+  type: FETCH_CHARACTERS,
+  payload: getCharacters(),
+  pendingType: FETCH_CHARACTERS_LOADING
+});
--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import { promiseMiddleware } from 'promise-middleware-redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(promiseMiddleware)
   )
 );
