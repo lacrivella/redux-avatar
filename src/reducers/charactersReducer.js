b9e548f3804e1dac166e67e3897ae7494dd2bc76
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-30 15:01:34 -0700

    gd typo it works now

--- src/reducers/charactersReducer.js
+++ src/reducers/charactersReducer.js
@@ -11,7 +11,7 @@ export default function reducer(state = initialState, action) {
     case FETCH_CHARACTERS_LOADING:
       return { ...state, loading: true };
     case FETCH_CHARACTERS:
-      return { ...state, loading: false,  error: null, characters: action.payloads };
+      return { ...state, loading: false,  error: null, characters: action.payload };
     default:
       return state;
   }
