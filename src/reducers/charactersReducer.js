25e4564a43812a0e226b0c9d9556bc2a3cf06ea7
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-30 14:10:33 -0700

    created reducer and added to combine reducers

---
+++ src/reducers/charactersReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_CHARACTERS, FETCH_CHARACTERS_LOADING } from '../actions/characterActions';
+
+const initialState = {
+  characters: [],
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_CHARACTERS_LOADING:
+      return { ...state, loading: true };
+    case FETCH_CHARACTERS:
+      return { ...state, loading: false,  error: null, characters: action.payloads };
+    default:
+      return state;
+  }
+}
